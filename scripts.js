c1b7f2cbbf66063bae32bda594a3ace6847489a4
Julio Muller <juliodolago@gmail.com>
2020-10-27 01:42:33 -0300

    Created Vue app

---
+++ scripts.js
@@ -0,0 +1,38 @@
+Vue.createApp({
+  setup() {
+    const emojis = Vue.ref({})
+    const searchText = Vue.ref('')
+    const notification = Vue.ref('')
+    const notificationTimeout = Vue.ref(null)
+
+    const copyToClipboard = (text) => {
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          clearTimeout(notificationTimeout.value)
+          notification.value = text
+          notificationTimeout.value = setTimeout(() => {
+            notification.value = ''
+          }, 2000)
+        })
+    }
+
+    const searchedEmojis = Vue.computed(() => {
+      return Object
+        .keys(emojis.value)
+        .filter((key) => (key.indexOf(searchText.value) > -1))
+        .map((key) => ({ name: `:${key}:`, url: emojis.value[key] }))
+    })
+
+    axios
+      .get('https://api.github.com/emojis')
+      .then((response) => (emojis.value = response.data))
+
+    return {
+      copyToClipboard,
+      searchedEmojis,
+      searchText,
+      notification,
+    }
+  }
+}).mount('#app')
