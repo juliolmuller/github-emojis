a87b5b6e3db98cee2db93f8b8fe5a4719007e5bb
Julio Muller <juliodolago@gmail.com>
2021-06-10 00:51:00 -0300

    Create Nuxt project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'lacussoft',
+    'lacussoft/typescript',
+    '@nuxtjs/typescript',
+    'plugin:nuxt/recommended'
+  ],
+  rules: {}
+}
