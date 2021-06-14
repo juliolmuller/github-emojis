92a0df5c2f32d586d8d968e7ccaa476686038554
Julio Muller <juliodolago@gmail.com>
2021-06-13 22:16:48 -0300

    Remove unused modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,12 +18,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Tells the root for source code: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-srcdir/
   srcDir: 'src/',
@@ -41,15 +39,8 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 }
