58d035311988c2dce30e6c1cee627011483c479b
Julio Muller <juliodolago@gmail.com>
2021-06-16 00:28:42 -0300

    Add link to GitHub repo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,14 +8,21 @@
         <figcaption>GitHub Emojis Search App</figcaption>
       </figure>
     </h1>
+    <span>
+      Developed with ♥ by <strong>Julio L. Muller</strong> •
+      <a href="https://github.com/juliolmuller/github-emojis" target="_blank">
+        Check out on <img src="/icon-github.svg" alt="GitHub">
+      </a>
+    </span>
   </header>
 </template>
 
 <style lang="scss">
 .app-header {
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
 
   padding: 8rem 0 6rem;
 
@@ -44,6 +51,40 @@
 
       figcaption {
         display: none;
+      }
+    }
+  }
+
+  & > span {
+    display: block;
+    margin-top: 1rem;
+
+    font-size: 1.5rem;
+
+    strong {
+      font-weight: 400;
+    }
+
+    a {
+      display: inline-flex;
+      align-items: center;
+      justify-content: space-between;
+
+      border-radius: 4px;
+      padding: 2px 4px;
+      color: white;
+      text-decoration: none;
+
+      transition: all 150ms;
+
+      &:hover {
+        filter: invert(1);
+        background: #000;
+      }
+
+      img {
+        height: 1.8rem;
+        margin-left: 0.75rem;
       }
     }
   }
