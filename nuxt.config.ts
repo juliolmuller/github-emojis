be26efcd14067c71cd49ce52fd7d828577b4c66b
juliolmuller <juliolmuller@outlook.com>
2025-01-26 23:16:14 -0300

    chore(deps): upgrade project to Vue v3 and Nuxt v3

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'GitHub Emojis Search App',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { hid: 'description', name: 'description', content: 'A straight forward search application for emojis supported by GitHub' },
+        { hid: 'keywords', name: 'keywords', content: 'github, emojis, markdown, github emojis, search' },
+        { hid: 'author', name: 'author', content: 'Julio L. Muller' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Chewy&family=Kufam:wght@700&family=Nanum+Gothic+Coding&display=swap' },
+      ],
+    },
+  },
+
+  css: [],
+  devtools: {
+    enabled: true,
+  },
+  srcDir: 'src/',
+
+  compatibilityDate: '2024-11-01',
+})
